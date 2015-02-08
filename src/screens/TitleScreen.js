d88ad8aa1a0d7dbce042987cf1c66d0f1b3fe9ee
Mr Speaker <mrspeaker@gmail.com>
2015-02-08 14:29:11 -0500

    better titlescreen

--- src/screens/TitleScreen.js
+++ src/screens/TitleScreen.js
@@ -2,7 +2,10 @@
 
 window.TitleScreen = {
 
-	count: 100,
+	count: 150,
+	textCount: 30,
+
+	state: "WAITING",
 
 	init: function (w, h, stage) {
 
@@ -14,26 +17,25 @@ window.TitleScreen = {
 			new PIXI.TilingSprite(main.textures["bg"], w, h)
 		);
 
-		var text = new PIXI.Text("Zilok", {
-			font:"70px 'Black Ops One', monospace",
-			fill:"#fff",
-			stroke: "#222",
-			strokeThickness: 5
-		});
-
-		text.position.x = 140;
-		text.position.y = 40;
-
-		var waiting = this.waiting = new PIXI.Text("Waiting for player 2...", {
-			font:"20px helvetica, arial, sans-serif", fill:"#777"
-		});
-		waiting.position.x = w / 2 - 100;
-		waiting.position.y = h / 2;
-		this.stage.addChild(text);
-		this.stage.addChild(waiting);
-
 		this.w = w;
 		this.h = h;
+
+		for (var j = 0; j < 6; j++) {
+			for (var i = 0; i < 30; i++) {
+				var tile = PIXI.Sprite.fromFrame("f" +(j >0 ? 4: 0) + "_" + 0);
+				tile.position.x = i * 16;
+				tile.position.y = 40 + (j * 16);
+				this.stage.addChild(tile);
+			}
+		}
+		for (j = 0; j < 3; j++) {
+			for (i = 0; i < 2; i++) {
+				var tile = PIXI.Sprite.fromFrame("f" + 0 + "_" + 0);
+				tile.position.x = (i === 0 ? 0 : 29) * 16;
+				tile.position.y = (j * 16) - 8;
+				this.stage.addChild(tile);
+			}
+		}
 
 		stage.addChild(this.stage);
 
@@ -43,9 +45,86 @@ window.TitleScreen = {
 
 	},
 
+	addTexts: function () {
+
+		var w = this.w,
+			h = this.h;
+
+		var text1 = new PIXI.Text("Z l  k", {
+			font:"70px 'Black Ops One', monospace",
+			fill:"#fff",
+			stroke: "#222",
+			strokeThickness: 5
+		});
+		var text2 = new PIXI.Text("  i o ", {
+			font:"70px 'Black Ops One', monospace",
+			fill:"#fff",
+			stroke: "#222",
+			strokeThickness: 5
+		});
+
+		text1.position.x = 160;
+		text2.position.x = 166;
+		this.text1 = text1;
+		this.text2 = text2;
+
+		var waiting = this.waiting = new PIXI.Text("Waiting for player 2...", {
+			font:"15pt 'Black Ops One', monospace",
+			stroke: "#333",
+			strokeThickness: 1,
+			fill:"#233F50"
+		});
+		waiting.position.x = w / 2 - (waiting.width / 2);
+		waiting.position.y = h - 60;
+
+		[
+			"by Mr Speaker.",
+			"Take out fuel and ammo supplies.",
+			"Destroy opponent's base for K/O."
+		].map (function (m, i) {
+			var t = new PIXI.Text(m, {
+				font:"15px 'Black Ops One', monospace",
+				fill:"#fff",
+				stroke: "#222",
+				strokeThickness: 5
+			});
+
+			t.x = w / 2 - (t.width / 2);
+			t.y = 190 + (i * 30);
+			this.stage.addChild(t);
+
+		}, this);
+
+		this.stage.addChild(text1);
+		this.stage.addChild(text2);
+		this.stage.addChild(waiting);
+
+
+	},
+
 	tick: function (dt) {
 
-		this.waiting.alpha = (Date.now() / 600) % 2 > 1 ? 1 : 0;
+		// Wait a bit, hopefully chrome's loaded the webfont!
+		if (this.textCount > 0) {
+			if (--this.textCount <= 0){
+				this.addTexts();
+			}
+			return;
+		}
+
+		if (this.state === "READY") {
+			if (this.count === 100) {
+				// REMOVE tings...
+				this.stage.removeChild(this.waiting);
+			}
+			if (this.count-- <= 0) {
+				this.listen("gameOn", this.data);
+			}
+		}
+
+		this.waiting.alpha = (Date.now() / 1000) % 3 < 2 ? 1 : 0;
+		this.text1.position.y = 55 + (Math.cos(Date.now() / 400) * 3)
+		this.text2.position.y = 59 + (Math.sin(Date.now() / 300) * 3)
 
 	},
 
@@ -57,6 +136,10 @@ window.TitleScreen = {
 
 		switch (e) {
 		case "net/game/start":
+			this.state = "READY";
+			this.data = data;
+			break;
+		case "gameOn":
 			main.setScreen(GameScreen);
 			var level = main.screen.level;
 			level.player = data.p1 === Network.socket.id ? level.tank1 : level.tank2;
